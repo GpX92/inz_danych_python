916ce24d4bc9a4df6f16fd0e21fdf10a93178e48
GpX92 <gicolct@gmail.com>
2025-03-16 14:41:42 +0100

    Update load.py

--- loaders/load.py
+++ loaders/load.py
@@ -1,1 +1,8 @@
+import json
 
+def load(names, path):
+  with open(path, mode="w") as file_handler:
+    json_data = [
+        json.dumps({"Imię": name}) + "\n" for name in names
+    ]
+    file_handler.writelines(json_data)
